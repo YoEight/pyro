b47d345cee9d724e4f3dc904154176ac2a1613de
YoEight <yo.eight@gmail.com>
2023-06-23 23:42:50 -0400

    Implement smart constructors for constructing universally quantified types.

--- pyro-runtime/src/engine.rs
+++ pyro-runtime/src/engine.rs
@@ -4,7 +4,7 @@ use crate::value::{Channel, RuntimeValue};
 use crate::PyroLiteral;
 use pyro_core::annotate::Ann;
 use pyro_core::ast::{Abs, Pat, Proc, Prop, Record, Tag, Val};
-use pyro_core::{Dict, Knowledge, Type, TypePointer, STDLIB};
+use pyro_core::{Dict, Knowledge, LocalScope, Type, TypePointer, STDLIB};
 use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::mpsc;
@@ -33,6 +33,109 @@ impl Default for EngineBuilder {
             runtime_values: Default::default(),
             env: None,
         }
+    }
+}
+
+pub struct TypeBuilder<'a> {
+    know: &'a mut Knowledge,
+}
+
+impl<'a> TypeBuilder<'a> {
+    pub fn new(know: &'a mut Knowledge) -> TypeBuilder<'a> {
+        Self { know }
+    }
+
+    pub fn for_all(self, var_name: impl AsRef<str>) -> ForAllVarBuilder<'a> {
+        let binders = vec![var_name.as_ref().to_string()];
+        let scope = self.know.new_scope(&STDLIB);
+        let var_dict = Dict::new(Type::named(var_name.as_ref()));
+        let var = self
+            .know
+            .declare_from_dict(&scope, var_name.as_ref(), var_dict);
+        let mut vars = HashMap::new();
+
+        vars.insert(var_name.as_ref().to_string(), var.clone());
+
+        let for_all = ForAllBuilder {
+            know: self.know,
+            scope,
+            binders,
+            constraints: vec![],
+            vars,
+        };
+
+        ForAllVarBuilder {
+            var,
+            inner: for_all,
+        }
+    }
+}
+
+pub struct ForAllBuilder<'a> {
+    know: &'a mut Knowledge,
+    scope: LocalScope,
+    binders: Vec<String>,
+    constraints: Vec<TypePointer>,
+    vars: HashMap<String, TypePointer>,
+}
+
+impl<'a> ForAllBuilder<'a> {
+    pub fn add_var(mut self, var_name: impl AsRef<str>) -> ForAllVarBuilder<'a> {
+        let scope = self.scope.clone();
+        let var_dict = Dict::new(Type::named(var_name.as_ref()));
+        let var = self
+            .know
+            .declare_from_dict(&scope, var_name.as_ref(), var_dict);
+
+        self.binders.push(var_name.as_ref().to_string());
+        self.vars.insert(var_name.as_ref().to_string(), var.clone());
+
+        ForAllVarBuilder { var, inner: self }
+    }
+
+    pub fn body(self, var_name: impl AsRef<str>) -> eyre::Result<TypePointer> {
+        if let Some(pointer) = self.vars.get(var_name.as_ref()) {
+            let body = if self.constraints.is_empty() {
+                pointer.clone()
+            } else {
+                TypePointer::Qual(self.constraints, Box::new(pointer.clone()))
+            };
+
+            return Ok(TypePointer::ForAll(
+                false,
+                self.scope,
+                self.binders,
+                Box::new(body),
+            ));
+        }
+
+        eyre::bail!(
+            "Type variable {} doesn't exist in this forall type expression",
+            var_name.as_ref()
+        )
+    }
+}
+
+pub struct ForAllVarBuilder<'a> {
+    var: TypePointer,
+    inner: ForAllBuilder<'a>,
+}
+
+impl<'a> ForAllVarBuilder<'a> {
+    pub fn done(self) -> ForAllBuilder<'a> {
+        self.inner
+    }
+
+    pub fn add_constraint(mut self, constraint: impl AsRef<str>) -> eyre::Result<Self> {
+        if let Some(constr) = self.inner.know.look_up(&STDLIB, constraint.as_ref()) {
+            self.inner
+                .constraints
+                .push(TypePointer::app(constr, self.var.clone()));
+
+            return Ok(self);
+        }
+
+        eyre::bail!("Constraint '{}' doesn't exist", constraint.as_ref())
     }
 }
 
@@ -83,29 +186,18 @@ impl EngineBuilder {
     }
 
     pub fn stdlib(mut self, env: Env) -> Self {
-        let print_forall_scope = self.knowledge.new_scope(&STDLIB);
-        let print_forall_var_type = Dict::new(Type::named("'a"));
-        let print_forall_var =
-            self.knowledge
-                .declare_from_dict(&print_forall_scope, "'a", print_forall_var_type);
+        let print_param_type = TypeBuilder::new(&mut self.knowledge)
+            .for_all("'a")
+            .add_constraint("Show")
+            .unwrap()
+            .done()
+            .body("'a")
+            .unwrap();
+
         self.knowledge.declare_from_pointer(
             &STDLIB,
             "print",
-            TypePointer::app(
-                self.knowledge.client_pointer(),
-                TypePointer::ForAll(
-                    false,
-                    print_forall_scope.clone(),
-                    vec!["'a".to_string()],
-                    Box::new(TypePointer::Qual(
-                        vec![TypePointer::app(
-                            self.knowledge.show_pointer(),
-                            print_forall_var.clone(),
-                        )],
-                        Box::new(print_forall_var),
-                    )),
-                ),
-            ),
+            TypePointer::app(self.knowledge.client_pointer(), print_param_type),
         );
 
         self.runtime_values.insert(
