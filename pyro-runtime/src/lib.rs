703a0ff7614d62269fa39a77fb8d43891ac16d90
YoEight <yo.eight@gmail.com>
2023-06-24 21:20:45 -0400

    Export PyroProcess type.

--- pyro-runtime/src/lib.rs
+++ pyro-runtime/src/lib.rs
@@ -3,7 +3,7 @@ mod env;
 mod runtime;
 mod value;
 
-pub use engine::{Engine, EngineBuilder};
+pub use engine::{Engine, EngineBuilder, PyroProcess};
 pub use env::Env;
 pub use runtime::Runtime;
 pub use value::{Channel, PyroValue, RuntimeValue};
