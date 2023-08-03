870db51bc3cc4ee9af23ccfcddc913ca13f28e09
YoEight <yo.eight@gmail.com>
2023-08-02 23:03:03 -0400

    Upgrade to latest glyph version.

--- pyro-repl/src/main.rs
+++ pyro-repl/src/main.rs
@@ -1,6 +1,6 @@
 use clap::{Parser, Subcommand};
 use directories::UserDirs;
-use glyph::{Input, Options};
+use glyph::{FileBackedInputs, Input, Options, PromptOptions};
 use pyro_core::annotate::{annotate_decl, annotate_val};
 use pyro_core::ast::Tag;
 use pyro_core::parser::ParserState;
@@ -8,10 +8,10 @@ use pyro_core::sym::Sym;
 use pyro_core::tokenizer::Tokenizer;
 use pyro_core::{infer_decl, infer_val, Machine};
 use pyro_runtime::{Engine, Env};
+use std::io;
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
-#[command(name = ":")]
 struct Shell {
     #[command(subcommand)]
     cmd: Cmd,
@@ -29,19 +29,38 @@ enum Cmd {
     Exit,
 }
 
+#[derive(Default)]
+struct State {
+    modules: Vec<String>,
+}
+
+impl State {
+    fn next_input(&self, inputs: &mut FileBackedInputs) -> io::Result<Option<Input<Shell>>> {
+        let prompt = self.modules.join(" ");
+        let options = if prompt.is_empty() {
+            PromptOptions::default()
+        } else {
+            PromptOptions::default().prompt(prompt)
+        };
+
+        inputs.next_input_with_parser_and_options::<Shell>(&options)
+    }
+}
+
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     let user_dirs = UserDirs::new();
     let options = Options::default()
-        .prompt("π")
+        .prompt("π>")
         .header(include_str!("header.txt"))
         .author("Yo Eight")
         .version("master");
 
     let mut inputs = glyph::file_backed_inputs(options, ".pyro-repl")?;
     let mut engine = Engine::with_nominal_typing().stdlib(Env::stdio()).build()?;
+    let mut state = State::default();
 
-    while let Some(input) = inputs.next_input_with_parser::<Shell>()? {
+    while let Some(input) = state.next_input(&mut inputs)? {
         match input {
             Input::Exit => {
                 break;
@@ -55,14 +74,14 @@ async fn main() -> eyre::Result<()> {
                         PathBuf::from(file)
                     };
 
-                    if let Err(e) = add_module(&mut engine, path) {
+                    if let Err(e) = add_module(&mut state, &mut engine, path) {
                         println!("ERR: {}", e);
                     }
                 }
 
                 Cmd::Type { expr } => {
                     if let Err(e) = type_expr(&mut engine, expr) {
-                        println!("Err: {}", e);
+                        println!("ERR: {}", e);
                     }
                 }
 
@@ -104,8 +123,12 @@ fn extrapolate_path(dirs: &UserDirs, path: PathBuf) -> PathBuf {
     buf
 }
 
-fn add_module<M: Machine>(engine: &mut Engine<M>, path: PathBuf) -> eyre::Result<()> {
-    let source_code = std::fs::read_to_string(path)?;
+fn add_module<M: Machine>(
+    state: &mut State,
+    engine: &mut Engine<M>,
+    path: PathBuf,
+) -> eyre::Result<()> {
+    let source_code = std::fs::read_to_string(path.as_path())?;
     let tokens = Tokenizer::new(source_code.as_str()).tokenize()?;
     let mut parser = ParserState::new(tokens.as_slice());
 
@@ -128,6 +151,8 @@ fn add_module<M: Machine>(engine: &mut Engine<M>, path: PathBuf) -> eyre::Result
             break;
         }
     }
+
+    state.modules.push(format!("{}", path.to_string_lossy()));
 
     Ok(())
 }
